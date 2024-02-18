a80a16e02056ad38c73635237cade77b38abe701
developersaddam <developerssaddam@gmail.com>
2024-02-19 03:14:14 +0600

    Added sell ticket data show with create element and don't click again selected btn

--- js/functions.js
+++ js/functions.js
@@ -12,10 +12,16 @@ for (const seat of allSeat) {
     const totalSellSeat = getInnerText("totalSellSeat");
     setInnerText("totalSellSeat", totalSellSeat + 1);
 
-    
-
     // Change-Btn-backgroundColor.
     changeBg(targetBtn.innerText);
+
+    // Validation btn again click.
+    if (targetBtn.classList.contains("bgGreen")) {
+      targetBtn.setAttribute("disabled", true);
+    }
+
+    // Show seat name and price in dataTable.
+    createTr(targetBtn.innerText, "tbody");
   });
 }
 
@@ -39,4 +45,22 @@ function changeBg(elementId) {
   const targetElement = document.getElementById(elementId);
   targetElement.classList.remove("bgGray");
   targetElement.classList.add("bgGreen");
+}
+
+// Create tr function.
+function createTr(btnText, elementId) {
+  const tr = document.createElement("tr");
+  const td1 = document.createElement("td");
+  const td2 = document.createElement("td");
+  const td3 = document.createElement("td");
+
+  td1.innerText = btnText;
+  td2.innerText = "Economoy";
+  td3.innerText = "550";
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+
+  const tbody = document.getElementById(elementId);
+  tbody.appendChild(tr);
 }
